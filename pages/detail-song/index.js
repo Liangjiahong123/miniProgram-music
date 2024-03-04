30b622b83f9f3ebbbc8f935d8d94bc2196938479
jiahong <2621762128@qq.com>
2024-03-04 15:49:59 +0800

    feat

--- pages/detail-song/index.js
+++ pages/detail-song/index.js
@@ -4,11 +4,23 @@ Page({
   onLoad(options) {
     const type = options.type;
     this.useSongStore(type);
+    this.setPageTitle();
   },
+
   useSongStore(type) {
     this.storeBindings = createStoreBindings(this, {
       store: songStore,
-      fields: [type]
+      fields: {
+        songsData: () => songStore[type]
+      }
+    });
+  },
+
+  setPageTitle() {
+    wx.nextTick(() => {
+      wx.setNavigationBarTitle({
+        title: this.data.songsData?.name
+      });
     });
   }
 });
